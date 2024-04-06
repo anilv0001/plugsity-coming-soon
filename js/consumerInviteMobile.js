37f932a46cb82f6fe1c7efc30a81f38c0381821b
Anil Vishwakarma <anilv0001@gmail.com>
2024-04-06 21:15:29 +0530

    change the api link

--- js/consumerInviteMobile.js
+++ js/consumerInviteMobile.js
@@ -299,7 +299,7 @@ function handleMobileSubmit() {
     // zipCode.value = "";
     console.log("isFormValid", data);
     postData(
-      "https://cors-anywhere.herokuapp.com/https://plugsity.herokuapp.com/api/CustomerInvitations",
+      "https://api.cs.plugsity.com/avi/v1/CustomerInvitations",
       data
     )
       .then((data) => {
